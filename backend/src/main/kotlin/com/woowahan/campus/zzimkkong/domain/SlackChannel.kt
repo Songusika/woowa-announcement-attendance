5cbec7df51d5ee55d4e23cbfffec42fe2e392eeb
최준호 <junho5336@gmail.com>
2023-10-24 16:28:31 +0900

    feat: 캠퍼스 수정 API 추가

--- backend/src/main/kotlin/com/woowahan/campus/zzimkkong/domain/SlackChannel.kt
+++ backend/src/main/kotlin/com/woowahan/campus/zzimkkong/domain/SlackChannel.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("SLACK_CHANNEL")
-class SlackChannel (
+class SlackChannel(
     val url: String,
     val campusId: Long,
     @Id val id: Long = 0L,
