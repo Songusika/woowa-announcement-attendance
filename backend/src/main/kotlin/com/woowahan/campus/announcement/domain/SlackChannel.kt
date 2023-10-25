5d011bb405a59988161ae36a170a7135ad76760c
songusika <sws981202@gmail.com>
2023-10-25 13:50:01 +0900

    feat: 슬랙 채널 도메인 구현

---
+++ backend/src/main/kotlin/com/woowahan/campus/announcement/domain/SlackChannel.kt
@@ -0,0 +1,9 @@
+package com.woowahan.campus.announcement.domain
+
+import com.woowahan.campus.announcement.support.BaseRootEntity
+
+class SlackChannel(
+    val providerId: String,
+    val name: String,
+    id: Long = 0L,
+) : BaseRootEntity<SlackChannel>(id)
