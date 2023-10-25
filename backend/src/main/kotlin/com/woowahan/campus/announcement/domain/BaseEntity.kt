4169493e0e03c82041518b1617d51161297f9ffa
songusika <sws981202@gmail.com>
2023-10-25 13:46:04 +0900

    feat: BaseRootEntity 구현

--- backend/src/main/kotlin/com/woowahan/campus/announcement/domain/BaseEntity.kt
+++ backend/src/main/kotlin/com/woowahan/campus/announcement/domain/BaseEntity.kt
@@ -1,11 +1,30 @@
 package com.woowahan.campus.announcement.domain
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.domain.AbstractAggregateRoot
 
 abstract class BaseEntity(
     @Id
-    val id: Long
+    val id: Long,
 ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BaseEntity
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
+
+abstract class BaseRootEntity<T : AbstractAggregateRoot<T>>(
+    @Id
+    val id: Long = 0L,
+) : AbstractAggregateRoot<T>() {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
