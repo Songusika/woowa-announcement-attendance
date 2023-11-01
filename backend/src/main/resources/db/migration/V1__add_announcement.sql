75e549c25f047a38261b1c833d4b647218aac765
songusika <sws981202@gmail.com>
2023-11-01 23:07:38 +0900

    refactor: 작성자 ddl 수정

--- backend/src/main/resources/db/migration/V1__add_announcement.sql
+++ backend/src/main/resources/db/migration/V1__add_announcement.sql
@@ -3,8 +3,9 @@ create table announcement
     id               bigint auto_increment primary key,
     title            varchar(255)   not null,
     content          varchar(65535) not null,
-    author           varchar(65535) not null,
-    slack_channel_id bigint         not null
+    author           varchar(20) not null,
+    slack_channel_id bigint         not null,
+    created_at       datetime       not null
 );
 
 create table campus
