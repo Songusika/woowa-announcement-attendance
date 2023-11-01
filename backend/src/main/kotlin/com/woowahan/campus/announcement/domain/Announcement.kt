c78103c08421b92e65a73e44e89fdecd06a526b1
songusika <sws981202@gmail.com>
2023-11-02 00:18:20 +0900

    refactor: BaseEntity / BaseRootEntity Jpa 교체

--- backend/src/main/kotlin/com/woowahan/campus/announcement/domain/Announcement.kt
+++ backend/src/main/kotlin/com/woowahan/campus/announcement/domain/Announcement.kt
@@ -1,7 +1,8 @@
 package com.woowahan.campus.announcement.domain
 
-import com.woowahan.campus.announcement.support.BaseEntity
+import com.woowahan.campus.announcement.support.BaseRootEntity
 import jakarta.persistence.Entity
+import java.time.LocalDateTime
 
 @Entity
 class Announcement(
@@ -9,7 +10,6 @@ class Announcement(
     val content: String,
     val author: String,
     val slackChannelId: Int,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
     id: Long = 0L,
-) : BaseEntity(id) {
-    fun withId(id: Long): Announcement = Announcement(title, content, author, slackChannelId, id)
-}
+) : BaseRootEntity<Announcement>(id)
