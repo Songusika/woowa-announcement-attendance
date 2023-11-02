e3fe4f25c83f34dde97c8bb18b1110d067b05b3b
최준호 <junho5336@gmail.com>
2023-11-02 13:34:35 +0900

    feat: Setting 시간 검증 추가

--- backend/src/main/kotlin/com/woowahan/campus/zzimkkong/domain/Setting.kt
+++ backend/src/main/kotlin/com/woowahan/campus/zzimkkong/domain/Setting.kt
@@ -18,6 +18,15 @@ class Setting(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0L,
 ) {
+    init {
+        validateTime(startTime, endTime)
+    }
+
+    private fun validateTime(startTime: LocalTime, endTime: LocalTime) {
+        require(!startTime.isAfter(endTime)) { "시작 시간이 종료 시간보다 늦을 수 없습니다." }
+        require(startTime != endTime) { "시작 시간과 종료 시간이 같을 수 없습니다." }
+    }
+
     fun getEnableDays(): List<DayOfWeek> {
         return enableDays.split(",")
             .map { DayOfWeek.valueOf(it) }
---
+++ backend/src/test/kotlin/com/woowahan/campus/zzimkkong/domain/SettingTest.kt
@@ -0,0 +1,51 @@
+package com.woowahan.campus.zzimkkong.domain
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import java.time.LocalTime
+
+class SettingTest : StringSpec({
+
+    "시작 시간이 종료 시간보다 빠르면 예외가 발생하지 않는다." {
+        val startTime = LocalTime.of(9, 0)
+        val endTime = LocalTime.of(10, 0)
+
+        Setting(
+            spaceId = 1L,
+            startTime = startTime,
+            endTime = endTime,
+            maximumMinute = 30,
+            enableDays = "MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY"
+        )
+    }
+
+    "시작 시간이 종료 시간보다 늦으면 예외가 발생한다." {
+        val startTime = LocalTime.of(10, 0)
+        val endTime = LocalTime.of(9, 0)
+
+        shouldThrow<IllegalArgumentException> {
+            Setting(
+                spaceId = 1L,
+                startTime = startTime,
+                endTime = endTime,
+                maximumMinute = 30,
+                enableDays = "MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY"
+            )
+        }
+    }
+
+    "시작 시간과 종료 시간이 같으면 예외가 발생한다." {
+        val startTime = LocalTime.of(9, 0)
+        val endTime = LocalTime.of(9, 0)
+
+        shouldThrow<IllegalArgumentException> {
+            Setting(
+                spaceId = 1L,
+                startTime = startTime,
+                endTime = endTime,
+                maximumMinute = 30,
+                enableDays = "MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY"
+            )
+        }
+    }
+})
