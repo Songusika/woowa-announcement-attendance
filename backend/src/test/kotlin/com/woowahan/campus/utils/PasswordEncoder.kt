eca135b1671c295dc8040eb883477d08203ae268
songusika <sws981202@gmail.com>
2023-11-02 13:57:58 +0900

    refactor: 코드 정렬

--- backend/src/test/kotlin/com/woowahan/campus/utils/PasswordEncoder.kt
+++ backend/src/test/kotlin/com/woowahan/campus/utils/PasswordEncoder.kt
@@ -3,5 +3,5 @@ package com.woowahan.campus.utils
 import java.util.*
 
 fun basicEncodePassword(password: String): String {
-    return "Basic "+String(Base64.getEncoder().encode(password.toByteArray()));
+    return "Basic "+String(Base64.getEncoder().encode(password.toByteArray()))
 }
