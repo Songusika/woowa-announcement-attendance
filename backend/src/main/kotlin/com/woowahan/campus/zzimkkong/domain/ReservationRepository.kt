550a54dc902dd915c46f415d74099151c8e62dcb
최준호 <junho5336@gmail.com>
2023-11-08 18:06:05 +0900

    refactor: 미사용 클래스 제거

--- backend/src/main/kotlin/com/woowahan/campus/zzimkkong/domain/ReservationRepository.kt
+++ backend/src/main/kotlin/com/woowahan/campus/zzimkkong/domain/ReservationRepository.kt
@@ -1,6 +1,5 @@
 package com.woowahan.campus.zzimkkong.domain
 
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.Repository
 import java.time.LocalDate
 
@@ -14,9 +13,6 @@ interface ReservationRepository : Repository<Reservation, Long> {
     fun findAllBySpaceIdAndDate(spaceId: Long, date: LocalDate): List<Reservation>
 
     fun delete(reservation: Reservation)
-
-    @Query("select (count(r) > 0) from Reservation r")
-    fun existsByStartTimeAndEndTime(): Boolean
 
     fun findAllBySpaceIdIn(spaceIds: List<Long>): List<Reservation>
 }
